d148ffb7ae4091bf074a7fdd1e2c990a422020c4
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-08-06 12:03:05 -0400

    tets

--- test/model.specs.js
+++ test/model.specs.js
@@ -1,9 +1,13 @@
 global.window = global;
 global.assert = require('chai').assert;
 require('../src/js/model');
+var chai = require('chai');
+chai.use(require('chai-dom'));
 
 describe('Crear título de lista', () => {
   it('Debe retornar un string', () => {
+    document.querySelector('section').should.have.id('getTitleList')
+    expect(document.querySelector('section')).to.have.id('foo')
     const titleList = 'abcdef';
     const result = window.model.titleList()
     assert.equal(typeof result, 'string')
